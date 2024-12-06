ae59188512be0632b37f7cd44a010d0a960e93b6
Vlad Solomon <vladsolomon720@gmail.com>
2024-12-06 20:21:46 +0200

    create /videos endpoint

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require("express");
+const youtube = require("scrape-youtube");
+
 require("dotenv").config();
 
 const app = express();
 
 app.get("/", (_, res) => {
 	res.json({ "@meta": "Welcome to the vide-with-cat server!" });
+});
+
+app.get("/videos", async (req, res) => {
+	const { title } = req.query;
+	const { videos } = await youtube.search(title, { type: "video", request: { headers: { "Accept-Language": "en" } } });
+
+	res.json(videos);
 });
 
 app.listen(process.env.PORT, () => {
