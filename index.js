f1bb253557d88148f7b2792e7e1afb60366040c4
Vlad Solomon <vladsolomon720@gmail.com>
2024-12-06 20:07:37 +0200

    create server

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+const express = require("express");
+require("dotenv").config();
 
+const app = express();
+
+app.get("/", (_, res) => {
+	res.json({ "@meta": "Welcome to the vide-with-cat server!" });
+});
+
+app.listen(process.env.PORT, () => {
+	console.log(`Running on http://localhost:${process.env.PORT}`);
+});
